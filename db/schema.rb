1e9ef0d5bfb1cfae3f807d08bb02976c150df4ff
Timothy Aveni <timothy@tacosareawesome.com>
2014-08-17 22:03:08 -0400

    Add Person model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807020201) do
+ActiveRecord::Schema.define(version: 20140818020258) do
 
   create_table "comments", force: true do |t|
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "post_id"
+  end
+
+  create_table "people", force: true do |t|
+    t.string   "name"
+    t.string   "key"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
