454ec9207c9f6313640250b3c5d632d9313f285d
Timothy Aveni <timothy@tacosareawesome.com>
2014-09-21 19:11:43 -0400

    Fix some issues with rails God I hate rails

--- app/assets/javascripts/authentication.js
+++
@@ -1,9 +0,0 @@
-var urlMatches = location.href.match(/#k=([0-9]+)\.([a-f0-9]+)/);
-
-if (urlMatches !== null) {
-	var uid = urlMatches[1];
-	var key = urlMatches[2];
-
-	localStorage.setItem("uid", uid);
-	localStorage.setItem("key", key);
-}
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'privatekey/authenticate'
-
-  resources :posts do
-    resources :comments
-  end
-
-  root 'posts#index'
-end
