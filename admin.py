62b2749d41544e700a5b821a876bdea81715fb90
Timothy Aveni <timothy@tacosareawesome.com>
2014-10-17 06:06:18 -0400

    Add Post to admin

--- admin.py
+++ admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from mysite.bridie.models import User
+from mysite.bridie.models import User, Post
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
 	pass
+class PostAdmin(admin.ModelAdmin):
+	pass
 admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
