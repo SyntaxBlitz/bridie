b87830443db77fe68c82d7a8fb6a36340db53c23
Timothy Aveni <timothy@tacosareawesome.com>
2014-10-21 20:40:32 -0400

    Only grab first ten posts
    duuude it lazy-evaluates so I can just use the python array slice syntax to slice a QuerySet before it even evaluates

--- views.py
+++ views.py
@@ -3,6 +3,6 @@ from mysite.models import Post
 
 # Create your views here.
 def index(request):
-	posts = Post.objects.all()
+	posts = Post.objects.all()[:10]
 	ctx = {'posts_list': posts}
 	return render(request, 'templates/index.html', ctx)
