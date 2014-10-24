bab58ccee30fbff1771db9e765567c5e9f00fe24
Timothy Aveni <timothy@tacosareawesome.com>
2014-10-23 21:11:37 -0400

    Add CommentAdmin

--- admin.py
+++ admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from mysite.bridie.models import User, Post
+from mysite.bridie.models import User, Post, Comment
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
 	pass
 class PostAdmin(admin.ModelAdmin):
 	pass
+class CommentAdmin(admin.ModelAdmin):
+	pass
 admin.site.register(User, UserAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
